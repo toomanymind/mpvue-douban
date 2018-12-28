29798dd46a95a456a8bcc61c32174097164e1cb7
toomanymind <851217479@qq.com>
2018-12-28 21:31:20 +0800

    v

--- src/main.js
+++ src/main.js
@@ -15,11 +15,11 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['^pages/splash/main'],
+    pages: ['pages/splash/main'],
     window: {
       navigationBarBackgroundColor: '#40586d',
       navigationBarTextStyle: 'white',
-      navigationBarTitleText: '电影 « 豆瓣',
+      navigationBarTitleText: '电影<豆瓣>',
       backgroundColor: '#f8f9fb',
       backgroundTextStyle: 'dark',
       enablePullDownRefresh: false
